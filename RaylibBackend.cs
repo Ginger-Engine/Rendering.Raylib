4908a2f538e8dd317603c723676244e9c600ba49
aleksey.pilov <aleksey.pilov@yandex.ru>
2025-06-04 20:27:50 +0300

    cameras

--- RaylibBackend.cs
+++ RaylibBackend.cs
@@ -1,4 +1,5 @@
-using System.Numerics;
+﻿using System.Numerics;
+using Engine.Rendering.Cameras;
 using Engine.Rendering.Textures;
 using Raylib_cs;
 
@@ -25,7 +26,28 @@ public class RaylibBackend : IRenderBackend
     public void DrawTexture(ITexture texture, Vector2 position)
     {
         if (texture is RaylibTexture texture2D) Raylib.DrawTexture(texture2D.Raw, (int)position.X, (int)position.Y, Color.Beige);
-        else throw new System.Exception("texture is not a RaylibTexture");
+        else throw new Exception("texture is not a RaylibTexture");
+    }
+
+    public void SetCamera(ICamera? camera)
+    {
+        if (camera == null)
+        {
+            Raylib.EndMode2D();
+            return;
+        }
+        if (camera is not RaylibCamera raylibCamera) throw new Exception("camera is not a RaylibCamera");
+        switch (camera.CameraType)
+        {
+            case ICamera.Type.Orthographic:
+                Raylib.BeginMode2D(raylibCamera.Camera);
+                break;
+            case ICamera.Type.ScreenSpace:
+                break;
+            default:
+                Raylib.EndMode2D();
+                break;
+        }
     }
 
     public void Render() {}
--- RaylibCamera.cs
+++ RaylibCamera.cs
@@ -1,14 +1,36 @@
+﻿using System.Numerics;
 using Engine.Rendering.Cameras;
+using Engine.Rendering.Layers;
 using Raylib_cs;
 
 namespace Engine.Rendering.RaylibBackend;
 
 public class RaylibCamera : ICamera
 {
-    private readonly Camera2D _camera;
+    public Camera2D Camera;
+    public Vector2 Position
+    {
+        get => Camera.Target;
+        set => Camera.Target = value;
+    }
+
+    public float Rotation
+    {
+        get => Camera.Rotation;
+        set => Camera.Rotation = value;
+    }
+
+    public float Zoom
+    {
+        get => Camera.Zoom;
+        set => Camera.Zoom = value;
+    }
+
+    public Layer[] Layers { get; set; }
+    public ICamera.Type CameraType { get; set; }
 
     public RaylibCamera()
     {
-        _camera = new Camera2D();
+        Camera = new Camera2D();
     }
 }
